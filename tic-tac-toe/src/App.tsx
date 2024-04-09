16dda4005db6dce2b0c5da5d963f24964d276158
kuboyumi <121540707+kuboyumi@users.noreply.github.com>
2024-04-10 04:35:54 +0900

    update: 勝利・手番の判定・表示ロジック追加
    - App.tsx

--- tic-tac-toe/src/App.tsx
+++ tic-tac-toe/src/App.tsx
@@ -2,11 +2,19 @@ import React, { useState } from 'react';
 import './App.css'
 
 export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
+  const [squares, setSquares] = useState<Array<string | null>>(Array(9).fill(null));
+  const winner: string | null = calculateWinner(squares);
+  let status: string;
+
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O')
+  }
 
   function handleClick(i: number) {
-    if (squares[i]) return;
+    if (squares[i] || calculateWinner(squares)) return;
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = 'X';
@@ -19,6 +27,7 @@ export default function Board() {
 
   return (
     <>
+      <div className='status'>{status}</div>
       <div className='board-row'>
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -40,4 +49,22 @@ export default function Board() {
 
 function Square({value, onSquareClick}: {value: string | null, onSquareClick: React.MouseEventHandler<HTMLButtonElement> | undefined}) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
+}
+
+function calculateWinner(squares: Array<string | null>) {
+  const lines: Array<Array<number>> = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) return squares[a];
+  }
+  return null;
 }

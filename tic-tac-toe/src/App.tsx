f17a9fb942e75de86a6c8442b6354724065c065f
kuboyumi <121540707+kuboyumi@users.noreply.github.com>
2024-04-10 03:54:34 +0900

    update: OX機能を追加
    App.tsx

--- tic-tac-toe/src/App.tsx
+++ tic-tac-toe/src/App.tsx
@@ -2,35 +2,42 @@ import React, { useState } from 'react';
 import './App.css'
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i: number) {
+    if (squares[i]) return;
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    setXIsNext(!xIsNext);
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
 }
 
-function Square() {
-  const [value, setValue] = useState<string | null>(null);
-
-  function handleClick() {
-    setValue('X');
-  }
-  
-  return (
-    <button className="square" onClick={handleClick}>{value}</button>
-  );
+function Square({value, onSquareClick}: {value: string | null, onSquareClick: React.MouseEventHandler<HTMLButtonElement> | undefined}) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
 }

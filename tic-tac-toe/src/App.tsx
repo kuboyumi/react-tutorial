e4c6ef4031f2d9e7f39729ac0bf439fb5785ea19
kuboyumi <121540707+kuboyumi@users.noreply.github.com>
2024-04-11 08:34:35 +0900

    feat: 巻き戻し機能追加
    App.tsx

--- tic-tac-toe/src/App.tsx
+++ tic-tac-toe/src/App.tsx
@@ -2,14 +2,34 @@ import React, { useState } from 'react';
 import './App.css'
 
 function Game() {
-  const [xIsNext, setXIsNext] = useState<boolean>(true);
   const [history, setHistory] = useState<Array<Array<string | null>>>([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState<number>(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares: Array<string | null>) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory: Array<Array<string | null>> = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
+
+  function jumpTo(nextMove: number) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
 
   return(
     <div className='game'>
@@ -17,14 +37,14 @@ function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className='game-info'>
-        <ol>{}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
 }
 export default Game;
 
-function Board({ xIsNext, squares, onPlay }: {xIsNext: boolean, squares: Array<string | null>, onPlay: (nextSquares: any) => void}) {
+function Board({ xIsNext, squares, onPlay }: {xIsNext: boolean, squares: Array<string | null>, onPlay: (nextSquares: Array<string | null>) => void}) {
   function handleClick(i: number) {
     if (calculateWinner(squares) || squares[i]) {
       return;
